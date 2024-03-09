9b791dbe9b8673330dc9d9b66bed3550e0d305ce
osmannyildiz <iamosmannyildiz@gmail.com>
2024-03-09 11:40:03 +0300

    generate keypair

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import "react-native-get-random-values";
+import { generateKeypair } from "./lib/web3";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const keypair = generateKeypair();
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.text}>Address: {keypair.publicKey.toBase58()}</Text>
+
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	text: {
+		textAlign: "center",
+	},
 });
