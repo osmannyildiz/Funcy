98658c5092ab389ea5c4cd531c5597fa1c29390e
osmannyildiz <iamosmannyildiz@gmail.com>
2024-03-09 11:54:57 +0300

    persist keypair

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,14 +1,41 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import "react-native-get-random-values";
-import { generateKeypair } from "./lib/web3";
+import { generateKeypair, loadKeypairFromJson } from "./lib/web3";
 
 export default function App() {
-	const keypair = generateKeypair();
+	const [keypair, setKeypair] = useState(null);
+
+	useEffect(() => {
+		checkKeypair();
+	}, []);
+
+	const checkKeypair = async () => {
+		const loadedKeypairJson = await AsyncStorage.getItem("keypair");
+		const loadedKeypair = loadedKeypairJson
+			? loadKeypairFromJson(loadedKeypairJson)
+			: null;
+		if (loadedKeypair) {
+			setKeypair(loadedKeypair);
+		} else {
+			const newKeypair = generateKeypair();
+			const newKeypairJson = JSON.stringify(newKeypair);
+			await AsyncStorage.setItem("keypair", newKeypairJson);
+			setKeypair(newKeypair);
+		}
+	};
 
 	return (
 		<View style={styles.container}>
-			<Text style={styles.text}>Address: {keypair.publicKey.toBase58()}</Text>
+			{keypair ? (
+				<Text style={styles.text}>Address: {keypair.publicKey.toBase58()}</Text>
+			) : (
+				<Text style={styles.text}>
+					Generating/loading wallet, please wait...
+				</Text>
+			)}
 
 			<StatusBar style="auto" />
 		</View>
--- mobile-app/lib/web3.js
+++ mobile-app/lib/web3.js
@@ -3,3 +3,11 @@ import { Keypair } from "@solana/web3.js";
 export function generateKeypair() {
 	return Keypair.generate();
 }
+
+export function loadKeypairFromJson(json) {
+	const keypairData = JSON.parse(json);
+	const secretKeyArray = Object.keys(keypairData._keypair.secretKey).map(
+		(k) => keypairData._keypair.secretKey[k]
+	);
+	return Keypair.fromSecretKey(Uint8Array.from(secretKeyArray));
+}
